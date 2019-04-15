0257a1a89bcf826644f2750d9333398da11ac55a
KevinKibet <kevinkipkosgei310@gmail.com>
2019-04-15 14:03:30 +0300

    Fetching the tasks from mongoDB

--- app.js
+++ app.js
@@ -2,8 +2,31 @@
  const path = require('path');
  const bodyParser = require('body-parser');
 
+ //port
+ const port = 3000 ;
+
  //initializing the app
  const app = new express();
+
+
+ //setting the connection 
+ const MongoClient = require('mongodb').MongoClient;
+ const url = 'mongodb://localhost:27017/TaskApp';
+
+ //connecting to mongodb
+
+ MongoClient.connect(url, (err, database)=>{
+
+ 	if(err) throw err;
+ 	console.log('Connected to mongodb');
+ 	db = database ;
+  Tasks = db.collection('tasks');
+
+    app.listen(port,()=>{
+console.log('listening on port 3000')
+ })
+
+ });
 
 //setting up bodyparser middleware
 
@@ -20,9 +43,14 @@ app.set('view engine', 'ejs');
 //The get requests
 
 app.get('/', (req,res)=>{
-	res.render('index');
+
+Tasks.find({}).toArray((err, tasks)=>{
+  if(err) {
+  	console.log(err);
+  }
+  res.render('index', {tasks:tasks});
 })
 
- app.listen(3000,()=>{
-console.log('listening on port 3000')
- })
+	
+})
+
